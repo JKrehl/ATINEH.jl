1a62062632ab97cbcb8c113a88f8aa6613caa9e2
JKrehl <JKrehl@users.noreply.github.com>
2017-03-22 20:20:08 +0100

    fixed IndexSupports again, now type-stable (#10)

--- src/IndexSupports.jl
+++ src/IndexSupports.jl
@@ -1,3 +1,4 @@
+using Base.Cartesian
 import Base: size, getindex, (*), (+), convert
 
 export IndexSupportPoint, IndexReturningArray, FlatIndexReturningArray, assemble
@@ -6,9 +7,13 @@ struct IndexSupportPoint{N,V,I<:Tuple{Vararg{Number}}}
     val::V
     idx::I
     IndexSupportPoint(value::V, index::I) where {N, V, I<:NTuple{N, Number}} = new{N,V,I}(value, index)
+    IndexSupportPoint{N}(value::V, index::I) where {N, V, I<:Tuple{Vararg{Number}}} = new{N,V,I}(value, index)
+    IndexSupportPoint{N,V,I}(value::V, index::I) where {N, V, I<:Tuple{Vararg{Number}}} = new{N,V,I}(value, index)
 end
 
-struct IndexReturningArray{N, V, I<:NTuple{N, Integer}} <: AbstractArray{Vector{IndexSupportPoint{N, V, I}},N}
+@generated convert{N, V, IN, I<:NTuple{IN}}(::Type{IndexSupportPoint{N,V,I}}, val::Number) = :(IndexSupportPoint{$N, $V, $I}($V(val), $I(@ntuple $IN i->1)))
+
+struct IndexReturningArray{N, V, I<:NTuple{N, Integer}} <: AbstractArray{Tuple{IndexSupportPoint{N, V, I}},N}
     _size::I
     IndexReturningArray{V}(_size::I) where {N,V,I<:NTuple{N, Integer}} = new{N,V,I}(_size)
     IndexReturningArray(_size::I) where {N,I<:NTuple{N, Integer}} = new{N,Float64,I}(_size)
@@ -17,9 +22,9 @@ end
 size(iar::IndexReturningArray) = iar._size
 size(iar::IndexReturningArray, i::Int) = iar._size[i]
 
-getindex{N,V}(iar::IndexReturningArray{N,V}, idx::Vararg{Int, N}) = [IndexSupportPoint(one(V), idx)]
+@inline getindex{N,V,I}(iar::IndexReturningArray{N,V,I}, idx::Vararg{Int, N}) = (IndexSupportPoint(one(V), idx),)
 
-struct FlatIndexReturningArray{N, V, I<:NTuple{N, Integer}} <: AbstractArray{Vector{IndexSupportPoint{N, V, Tuple{Int}}},N}
+struct FlatIndexReturningArray{N, V, I<:NTuple{N, Integer}} <: AbstractArray{Tuple{IndexSupportPoint{N, V, Tuple{Int}}},N}
     _size::I
     FlatIndexReturningArray{V}(_size::I) where {N,V,I<:NTuple{N, Integer}} = new{N,V,I}(_size)
     FlatIndexReturningArray(_size::I) where {N,I<:NTuple{N, Integer}} = new{N,Float64,I}(_size)
@@ -28,9 +33,9 @@ end
 size(fiar::FlatIndexReturningArray) = fiar._size
 size(fiar::FlatIndexReturningArray, i::Int) = fiar._size[i]
 
-getindex{N,V}(fiar::FlatIndexReturningArray{N,V}, idx::Vararg{Int, N}) = [IndexSupportPoint(one(V), (sub2ind(fiar, idx...),))]
+@inline getindex{N,V,I}(fiar::FlatIndexReturningArray{N,V,I}, idx::Vararg{Int, N}) = (IndexSupportPoint{N}(one(V), (sub2ind(fiar, idx...),)),)
 
-(*){N,V,I}(val, isp::IndexSupportPoint{N,V,I}) = IndexSupportPoint(val*isp.val, isp.idx)
-(+){N,V,I}(isps1::Vector{IndexSupportPoint{N,V,I}}, isps2::Vector{IndexSupportPoint{N,V,I}}) = vcat(isps1, isps2)
+(*){N,V,I}(val, isps::Tuple{Vararg{IndexSupportPoint{N,V,I}}}) = map(isp -> IndexSupportPoint(val*isp.val, isp.idx), isps)
+(+){N,V,I}(isps1::Tuple{Vararg{IndexSupportPoint{N,V,I}}}, isps2::Tuple{Vararg{IndexSupportPoint{N,V,I}}}) = (isps1..., isps2...)
 
-assemble{N,V,I}(isps::Vector{IndexSupportPoint{N,V,I}}) = ([isp.val for isp in isps], ([isp.idx[i] for isp in isps] for i in 1:N)...)
+assemble{N,V,I}(isps::Tuple{Vararg{IndexSupportPoint{N,V,I}}}) = ([isp.val for isp in isps], ([isp.idx[i] for isp in isps] for i in 1:N)...)
