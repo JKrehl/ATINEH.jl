b644c7507a7040fdda8fa44452cf472599ba7f3d
JKrehl <j.krehl@ifw-dresden.de>
2017-03-22 20:19:25 +0100

    fixed IndexSupports again, now type-stable

--- test/IndexSupports.jl
+++ test/IndexSupports.jl
@@ -1,17 +1,16 @@
 using Base.Test
 
 @testset "IndexSupport" begin
-    @test @inferred(IndexReturningArray((3,4,5))) isa AbstractArray{Vector{IndexSupportPoint{3,Float64, NTuple{3,Int}}},3}
-    @test @inferred(IndexReturningArray((3,4,5))[1,1,1]) == [ATINEH.IndexSupportPoint(1.0, (1, 1, 1))]
+    @test @inferred(IndexReturningArray((3,4,5))) isa AbstractArray{Tuple{IndexSupportPoint{3,Float64, NTuple{3,Int}}},3}
+    @test @inferred(IndexReturningArray((3,4,5))[1,1,1]) == (ATINEH.IndexSupportPoint(1.0, (1, 1, 1)),)
     @test size(IndexReturningArray((3,4,5)), 1) == 3
     @test size(IndexReturningArray((3,4,5))) == (3,4,5)
-    @test IndexReturningArray((3,4,5))[ConstantExterior{3}(), 1,1,1] == [ATINEH.IndexSupportPoint(1.0, (1, 1, 1)),]
-    @test IndexReturningArray((3,4,5))[ConstantExterior{3}(), -1,1,1] == []
+    @test IndexReturningArray((3,4,5))[ConstantExterior{3}(), 1,1,1] == (ATINEH.IndexSupportPoint(1.0, (1, 1, 1)),)
+    @test IndexReturningArray((3,4,5))[ConstantExterior{3}(), -1,1,1] == Tuple{ATINEH.IndexSupportPoint{3,Float64,Tuple{Int64,Int64,Int64}}}(0)
     @test size(FlatIndexReturningArray((3,4,5)), 1) == 3
     @test size(FlatIndexReturningArray((3,4,5))) == (3,4,5)
-    @test FlatIndexReturningArray((3,4,5))[ConstantExterior{3}(), 2,2,2] == [ATINEH.IndexSupportPoint(1.0, (17,))]
-    @test FlatIndexReturningArray((3,4,5))[ConstantExterior{3}(), -2,2,2] == ATINEH.IndexSupportPoint{3,Float64,NTuple{3,Int64}}[]
-    @test @inferred(IndexReturningArray((3,4,5))[LinearInterpolation{3}(), 1.2,1.3,1]) isa Vector{ATINEH.IndexSupportPoint{3,Float64,NTuple{3,Int64}}}
-    @test length(IndexReturningArray((3,4,5))[LinearInterpolation{3}(), 1.2,1.3,1]) == 4
+    @test FlatIndexReturningArray((3,4,5))[ConstantExterior{3}(), 2,2,2] == (ATINEH.IndexSupportPoint{3}(1.0, (17,)),)
+    @test FlatIndexReturningArray((3,4,5))[ConstantExterior{3}(), -2,2,2] == Tuple{ATINEH.IndexSupportPoint{3,Float64,Tuple{Int64}}}(0)
+    @test @inferred(IndexReturningArray((3,4,5))[LinearInterpolation{3}(), 1.2,1.3,1]) isa NTuple{4, ATINEH.IndexSupportPoint{3,Float64,NTuple{3,Int64}}}
     @test assemble(FlatIndexReturningArray((3,4,5))[LinearInterpolation{3}(), 1.2,1.3,1]) isa Tuple{Vector{Float64}, Vector{Int}}
 end
