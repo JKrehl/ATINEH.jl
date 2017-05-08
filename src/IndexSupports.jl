2ab1f5d81bb54d481ea7c99ada0e4cf3a235df2d
JKrehl <j.krehl@ifw-dresden.de>
2017-05-08 18:30:34 +0200

    refactoring of IndexSupports

--- src/IndexSupports.jl
+++ src/IndexSupports.jl
@@ -1,54 +1,47 @@
-using Base.Cartesian
-import Base: size, getindex, (*), (+), convert, first, tail, start, next, done, eltype, length
+import Base: (*), (+), size, getindex, eltype
+export IndexSupport, FlatIndexSupport, AbstractSupportReturningArray, SupportReturningArray
 
-export IndexSupportPoint, IndexReturningArray, FlatIndexSupportPoint, FlatIndexReturningArray
+abstract type AbstractIndexSupport{N,V,I} end
 
-struct IndexSupportPoint{N,V,I<:Tuple{Vararg{Number}}}
+struct IndexSupport{N,V,I<:NTuple{N, Number}} <: AbstractIndexSupport{N,V,I}
     val::V
     idx::I
-    IndexSupportPoint(value::V, index::I) where {N, V, I<:NTuple{N, Number}} = new{N,V,I}(value, index)
-    IndexSupportPoint{N}(value::V, index::I) where {N, V, I<:Tuple{Vararg{Number}}} = new{N,V,I}(value, index)
-    IndexSupportPoint{N,V,I}(value::V, index::I) where {N, V, I<:Tuple{Vararg{Number}}} = new{N,V,I}(value, index)
+    IndexSupport(val::V, idx::I) where {N,V,I<:NTuple{N, <:Number}} = new{N,V,I}(val, idx)
+    IndexSupport{N,V,I}(val, idx=ntuple(_->1, Val{N})) where {N,V,I<:NTuple{N, <:Number}} = new{N,V,I}(V(val), I(idx))
 end
+(*){IS<:IndexSupport, V<:Number}(val::V, isp::IS) = IndexSupport(val*isp.val, isp.idx)
 
-@generated convert{N, V, IN, I<:NTuple{IN}}(::Type{IndexSupportPoint{N,V,I}}, val::Number) = :(IndexSupportPoint{$N, $V, $I}($V(val), $I(@ntuple $IN i->1)))
-
-(+){ISP<:IndexSupportPoint}(As::Vararg{<:NTuple{M, ISP} where M, N} where N) = foldl((a,b) -> (a...,b...), As)#::Tuple{Vararg{ISP}}
-(*){ISP<:IndexSupportPoint}(val, isps::Tuple{Vararg{ISP}}) = map(isp -> IndexSupportPoint(val*isp.val, isp.idx), isps)
-
-struct IndexReturningArray{N, V, I<:NTuple{N, Integer}} <: AbstractArray{Tuple{IndexSupportPoint{N, V, I}},N}
-    _size::I
-    IndexReturningArray(::Type{V}, _size::I) where {N,V,I<:NTuple{N, Integer}} = new{N,V,I}(_size)
-end
-IndexReturningArray(_size::Tuple{Vararg{<:Integer}}) = IndexReturningArray(Float64, _size)
-IndexReturningArray{T}(::Type{T}, _size::Vararg{<:Integer}) = IndexReturningArray(T, _size)
-IndexReturningArray(_size::Vararg{<:Integer}) = IndexReturningArray(Float64, _size)
-
-size(iar::IndexReturningArray) = iar._size
-size(iar::IndexReturningArray, i::Int) = iar._size[i]
-
-@inline getindex{N,V,I}(iar::IndexReturningArray{N,V,I}, idx::Vararg{Int, N}) = IndexSupportPoints((IndexSupportPoint(one(V), idx),))
-
-struct FlatIndexSupportPoint{V,I<:Number}
+struct FlatIndexSupport{N,V,I<:Number} <: AbstractIndexSupport{N,V,I}
     val::V
     idx::I
-    FlatIndexSupportPoint(value::V, index::I) where {V, I<:Number} = new{V,I}(value, index)
+    FlatIndexSupport(::Type{Val{N}}, val::V, idx::I) where {N,V,I<:Number} = new{N,V,I}(val, idx)
+    FlatIndexSupport{N,V,I}(val, idx=1) where {N,V,I} = new{N,V,I}(V(val), I(idx))
+end
+(*){N, IS<:FlatIndexSupport{N}, V<:Number}(val::V, isp::IS) = FlatIndexSupport(Val{N}, val*isp.val, isp.idx)
+
+(+){IS<:AbstractIndexSupport}(a::Vararg{IS}) = a
+(+){IS<:AbstractIndexSupport, A<:NTuple{N, IS} where N, B<:NTuple{N, IS} where N}(a::A, b::B) = (a..., b...)
+(+){IS<:AbstractIndexSupport, A<:NTuple{N, IS} where N}(a::A, b::IS) = (a..., b)
+(+){IS<:AbstractIndexSupport, B<:NTuple{N, IS} where N}(a::IS, b::B) = (a, b...)
+(*){V<:Number, IS<:AbstractIndexSupport, NIS}(val::V, isps::NTuple{NIS, IS}) = map(i->val*i, isps)
+
+abstract type AbstractSupportReturningArray{N, IS<:AbstractIndexSupport{N}} <: AbstractArray{IS,N} end
+
+size(iar::A where A<:AbstractSupportReturningArray) = iar._size
+size(iar::A where A<:AbstractSupportReturningArray, i::Int) = iar._size[i]
+eltype{IS}(::A where {N, A<:AbstractSupportReturningArray{N, IS}}) = IS
+eltype{IS}(::Type{A} where {N, A<:AbstractSupportReturningArray{N, IS}}) = IS
+
+struct SupportReturningArray{N, IS<:AbstractIndexSupport{N}} <: AbstractSupportReturningArray{N, IS}
+    _size::NTuple{N, Int}
+    SupportReturningArray(::Type{IS}, _size::NTuple{N,<:Integer}) where {N, IS<:AbstractIndexSupport{N}} = new{N,IS}(NTuple{N,Int}(_size))
 end
 
-convert{V, I}(::Type{FlatIndexSupportPoint{V,I}}, val::Number) = FlatIndexSupportPoint(V(val), one(I))
+SupportReturningArray{N, V}(::Type{IndexSupport}, ::Type{V}, _size::NTuple{N,<:Integer}) = SupportReturningArray(IndexSupport{N,V,NTuple{N,Int}}, _size)
+SupportReturningArray{N}(::Type{IndexSupport}, _size::NTuple{N,<:Integer}) = SupportReturningArray(IndexSupport{N,Float64,NTuple{N,Int}}, _size)
 
-(+){ISP<:FlatIndexSupportPoint}(As::Vararg{<:NTuple{M, ISP} where M, N} where N) = foldl((a,b) -> (a...,b...), As)#::Tuple{Vararg{ISP}}
-(*){ISP<:FlatIndexSupportPoint}(val, isps::Tuple{Vararg{ISP}}) = map(isp -> FlatIndexSupportPoint(val*isp.val, isp.idx), isps)
+SupportReturningArray{N, V}(::Type{FlatIndexSupport}, ::Type{V}, _size::NTuple{N,<:Integer}) = SupportReturningArray(FlatIndexSupport{N,V,Int}, _size)
+SupportReturningArray{N}(::Type{FlatIndexSupport}, _size::NTuple{N,<:Integer}) = SupportReturningArray(FlatIndexSupport{N,Float64,Int}, _size)
 
-struct FlatIndexReturningArray{N, V, I<:NTuple{N, Integer}} <: AbstractArray{Tuple{FlatIndexSupportPoint{V, Int}},N}
-    _size::I
-    FlatIndexReturningArray(::Type{V}, _size::I) where {N,V,I<:NTuple{N, Integer}} = new{N,V,I}(_size)
-end
-FlatIndexReturningArray(_size::Tuple{Vararg{<:Integer}}) = FlatIndexReturningArray(Float64, _size)
-FlatIndexReturningArray{T}(::Type{T}, _size::Vararg{<:Integer}) = FlatIndexReturningArray(T, _size)
-FlatIndexReturningArray(_size::Vararg{<:Integer}) = FlatIndexReturningArray(Float64, _size)
-
-size(fiar::FlatIndexReturningArray) = fiar._size
-size(fiar::FlatIndexReturningArray, i::Int) = fiar._size[i]
-
-@inline getindex{N,V,I}(fiar::FlatIndexReturningArray{N,V,I}, idx::Vararg{Int, N}) = (FlatIndexSupportPoint(one(V), sub2ind(fiar, idx...)),)
+@inline getindex{N,V,I,IS<:IndexSupport{N,V,I}}(iar::SupportReturningArray{N,IS}, idx::Vararg{<:Integer, N}) = IS(one(V), I(idx))
+@inline getindex{N,V,I,IS<:FlatIndexSupport{N,V,I}}(iar::SupportReturningArray{N,IS}, idx::Vararg{<:Integer, N}) = IS(one(V), I(sub2ind(iar._size, idx...)))
--- test/IndexSupports.jl
+++ test/IndexSupports.jl
@@ -1,15 +1,13 @@
 using Base.Test
 
 @testset "IndexSupport" begin
-    @test @inferred(IndexReturningArray((3,4,5))) isa AbstractArray{Tuple{IndexSupportPoint{3,Float64, NTuple{3,Int}}},3}
-    @test @inferred(IndexReturningArray((3,4,5))[1,1,1]) == (ATINEH.IndexSupportPoint(1.0, (1, 1, 1)),)
-    @test size(IndexReturningArray((3,4,5)), 1) == 3
-    @test size(IndexReturningArray((3,4,5))) == (3,4,5)
-    @test IndexReturningArray((3,4,5))[ConstantExterior{3}(), 1,1,1] == (ATINEH.IndexSupportPoint(1.0, (1, 1, 1)),)
-    @test IndexReturningArray((3,4,5))[ConstantExterior{3}(), -1,1,1] == Tuple{ATINEH.IndexSupportPoint{3,Float64,Tuple{Int64,Int64,Int64}}}(0)
-    @test size(FlatIndexReturningArray((3,4,5)), 1) == 3
-    @test size(FlatIndexReturningArray((3,4,5))) == (3,4,5)
-    @test FlatIndexReturningArray((3,4,5))[ConstantExterior{3}(), 2,2,2] == (ATINEH.FlatIndexSupportPoint(1.0, 17),)
-    @test FlatIndexReturningArray((3,4,5))[ConstantExterior{3}(), -2,2,2] == Tuple{ATINEH.FlatIndexSupportPoint{Float64,Int64}}(0)
-    @test @inferred(IndexReturningArray((3,4,5))[LinearInterpolation{3}(), 1.2,1.3,1]) isa NTuple{4, ATINEH.IndexSupportPoint{3,Float64,NTuple{3,Int64}}}
+    @test @inferred(SupportReturningArray(IndexSupport, (3,4,5))) isa AbstractArray{IndexSupport{3,Float64,Tuple{Int64,Int64,Int64}}, 3}
+    @test @inferred(SupportReturningArray(IndexSupport, (3,4,5))[1,1,1]) == IndexSupport(1.0, (1, 1, 1))
+    @test size(SupportReturningArray(IndexSupport, (3,4,5)), 1) == 3
+    @test size(SupportReturningArray(IndexSupport, (3,4,5))) == (3,4,5)
+    @test SupportReturningArray(IndexSupport, (3,4,5))[ConstantExterior{3}(), 1,1,1] == IndexSupport(1.0, (1, 1, 1))
+    @test SupportReturningArray(IndexSupport, (3,4,5))[ConstantExterior{3}(), -1,1,1] == IndexSupport{3,Float64,Tuple{Int64,Int64,Int64}}(0)
+    @test SupportReturningArray(FlatIndexSupport, (3,4,5))[ConstantExterior{3}(), 2,2,2] == FlatIndexSupport(Val{3}, 1.0, 17)
+    @test SupportReturningArray(FlatIndexSupport, (3,4,5))[ConstantExterior{3}(), -2,2,2] == FlatIndexSupport{3,Float64,Int64}(0)
+    @test @inferred(SupportReturningArray(FlatIndexSupport, (3,4,5))[LinearInterpolation{3}(), 1.2,1.3,1]) isa NTuple{4, FlatIndexSupport{3,Float64,Int64}}
 end
