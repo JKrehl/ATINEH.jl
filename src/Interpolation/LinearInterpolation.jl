071a5c98fdea00018382f45d23cb96c90cae1059
JKrehl <korppi@posteo.org>
2018-03-16 16:50:20 +0100

    reworked LinearInterpolation, removed special treatment of integer indexes

--- src/Interpolation/LinearInterpolation.jl
+++ src/Interpolation/LinearInterpolation.jl
@@ -6,75 +6,75 @@ export LinearInterpolation
 struct LinearInterpolation <: AbstractInterpolation
 end
 
-@propagate_inbounds getindex(A::MappedArray_byMap{<:LinearInterpolation}, I::Vararg{<:Number}) = getindex(A, I)
-
-@generated function getindex{N, T, IT<:NTuple{N, Number}}(A::MappedArray{T,<:Any,<:Any,<:LinearInterpolation}, I::IT)
-    xs = ntuple(j -> Symbol("x_",j), Val{N})
-    ex = :(getindex(A.a, $(xs...)))
-    preexs = Expr[]
-
+@generated function getindex(A::MappedArray{T,<:Any,<:Any,<:LinearInterpolation}, I::Vararg{<:Number, N}) where {N,T}
+    
+    ex = :(@ncall $N getindex A.array i->x_i)
     for i in 1:N
-        x_, i_, r_, f_ = xs[i], Symbol("i_",i), Symbol("r_",i), Symbol("f_",i)
-        if IT.parameters[i] <: AbstractFloat
-            push!(preexs, :($f_ = floor(I[$i])), :($i_ = unsafe_trunc(Int, $f_)), :($r_ = T(I[$i]-$f_)))
-
-            ex = quote
-                +(let $x_ = $i_; (1-$r_)*$ex; end,
-                    let $x_ = $i_+1; $r_*$ex; end)
+        x_ = Symbol("x_", i)
+        idx_ = Symbol("idx_", i)
+        rdx_ = Symbol("rdx_", i)
+        ex = quote
+            local r1 = let $x_ = $idx_
+               (one($T)-$rdx_) * $ex
             end
-        else
-            ex = quote
-               let $x_ = I[$i]; $ex; end
+            local r2 = let $x_ = $idx_+1
+                $rdx_ * $ex
             end
+            r1+r2
         end
     end
-
+    
     quote
         $(Expr(:meta, :inline))
         $(Expr(:meta, :propagate_inbounds))
-        $(preexs...)
+        
+        @nexprs $N i -> begin
+            fdx_i = floor(I[i])
+            idx_i = unsafe_trunc(Int, fdx_i)
+            rdx_i = $T(I[i] - fdx_i)
+        end
+        
         $ex
     end
 end
 
 
 function setindex!(::MappedArray_byMap{<:LinearInterpolation}, _, ::Vararg{<:Number})
-    throw(ArgumentError("setindex! is ill defined for linear interpolation"))
+    throw(ArgumentError("setindex! is ill defined for LinearInterpolation"))
     nothing
 end
 
-@propagate_inbounds addindex!(A::MappedArray_byMap{<:LinearInterpolation}, val, I::Vararg{<:Number}) = addindex!(A, val, I)
-
-@generated function addindex!{N, IT<:NTuple{N, Number}}(A::MappedArray_byMap{<:LinearInterpolation}, val, I::IT)
-    xs = ((Symbol("x_",i) for i in 1:N)...)
-    ex = :(addindex!(A.a, val, $(xs...)))
-    preexs = Expr[]
+@generated function addindex!(A::MappedArray{T,<:Any,<:Any,<:LinearInterpolation}, val, I::Vararg{<:Number, N}) where {N,T}
+    
+    ex = :(@ncall $N addindex! A.array val i->x_i)
 
     for i in 1:N
-        x_, i_, r_, f_ = xs[i], Symbol("i_",i), Symbol("r_",i), Symbol("f_",i)
-        if IT.parameters[i] <: AbstractFloat
-            push!(preexs, :($f_ = floor(I[$i])), :($i_ = unsafe_trunc(Int, $f_)), :($r_ = I[$i]-$f_))
-
-            ex = quote
-                let $x_ = $i_, val=val*(1-$r_);
-                    $ex
-                end
-                let $x_ = $i_+1, val=val*$r_;
-                    $ex
-                end
+        x_ = Symbol("x_", i)
+        idx_ = Symbol("idx_", i)
+        rdx_ = Symbol("rdx_", i)
+        
+        ex = quote begin
+            let $x_ = $idx_, val=val*(1-$rdx_)
+                $ex
             end
-        else
-            ex = quote
-               let $x_ = I[$i]; $ex; end
+            let $x_ = $idx_+1, val=val*$rdx_
+                $ex
             end
-        end
+        end end
     end
-
+    
     quote
         $(Expr(:meta, :inline))
         $(Expr(:meta, :propagate_inbounds))
-        $(preexs...)
+
+        @nexprs $N i -> begin
+            fdx_i = floor(I[i])
+            idx_i = unsafe_trunc(Int, fdx_i)
+            rdx_i = $T(I[i] - fdx_i)
+        end
+        
         $ex
+        
         nothing
     end
 end
